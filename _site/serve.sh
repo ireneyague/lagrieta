2d606028d80fd17c51dfdfbab638ec471ac81498
alay <contacto@adappgeo.net>
2017-11-11 23:22:13 +0100

    añadida carpeta _site

---
+++ _site/serve.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#
+# serve.sh
+#
+# Copyleft (ↄ) 2016 jkirchartz <me@jkirchartz.com>
+#
+# Distributed under terms of the NPL (Necessary Public License) license.
+#
+
+bundle exec jekyll serve & #-wIP 8000 --host=0.0.0.0
